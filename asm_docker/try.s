8187018ce57b120e00b1989c4a318608355a34d9
delmesia <wendellsmesia@gmail.com>
2024-08-13 04:21:25 +0800

    added jmp instruction

--- asm_docker/try.s
+++ asm_docker/try.s
@@ -1,9 +1,9 @@
 .globl _start 
 .section .text 
 _start:
-	movb $0b00000001, %cl
-	movb $0b10000000, %ch
-	addb %cl, %ch
-	movq %rcx, %rdi
-	movq $60, %rax
+	jmp nextplace
+	movq $10, %rdi
+nextplace:
+	movq $23, %rdi
+	movq $60,  %rax
 	syscall
