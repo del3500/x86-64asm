bd93ad55a85aebe5ac509f89bd55c60cbcada515
delmesia <wendellsmesia@gmail.com>
2024-08-12 17:42:27 +0800

    updated cs.txt

--- asm_docker/sc.sh
+++ asm_docker/sc.sh
@@ -2,7 +2,7 @@
 
 # Define variables
 IMAGE_NAME="latest_be"  # The name of your Docker image
-LOCAL_DIR="/Users/del/desktop/dev/asm_docker"  # Your specified local directory
+LOCAL_DIR="/Users/del/desktop/dev/x86-64asm"  # Your specified local directory
 CONTAINER_DIR="/code"  # Absolute path inside the container
 
 # Run the Docker container
